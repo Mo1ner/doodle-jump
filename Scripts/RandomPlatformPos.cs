215b1d8f503a12d7b96f8d40268c3ddf575ac84a
Nikita Lutko <nikitalutsko75@gmail.com>
2023-02-20 21:07:28 +0300

    newspawn

--- Scripts/RandomPlatformPos.cs
+++ Scripts/RandomPlatformPos.cs
@@ -1,22 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class RandomPlatformPos : MonoBehaviour
 {
     [SerializeField] private GameObject _platform;
-    [SerializeField] private GameObject _player;
 
-    private GameObject _myPlat;
+    private void Start()
+    {
+
+    }
 
     private void Update()
     {
-        
+       
     }
 
-    private void OnCollisionEnter2D(Collision2D col)
+    void RandomSpawn()
     {
-        Instantiate(_platform, new Vector2(Random.Range(-2f, 2f), _player.transform.position.y + (14 + Random.Range(.5f, 1f))), Quaternion.identity);
-        Destroy(col.gameObject);
+        
     }
 }
