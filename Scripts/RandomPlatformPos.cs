7784997a418bafce646ff81704ff42396b9f3c04
Nikita Lutko <nikitalutsko75@gmail.com>
2023-03-01 20:03:44 +0300

    new spawner

--- Scripts/RandomPlatformPos.cs
+++ Scripts/RandomPlatformPos.cs
@@ -5,20 +5,25 @@ using UnityEngine;
 
 public class RandomPlatformPos : MonoBehaviour
 {
-    [SerializeField] private GameObject _platform;
 
-    private void Start()
+    [SerializeField] private GameObject platformPrefab;
+
+    [SerializeField] private int numberOfPlatforms = 200;
+    [SerializeField] private float levelWidth = 3f;
+    [SerializeField] private float minY = .2f;
+    [SerializeField] private float maxY = 1.5f;
+
+    // Use this for initialization
+    void Start()
     {
 
-    }
+        Vector3 spawnPosition = new Vector3();
 
-    private void Update()
-    {
-       
-    }
-
-    void RandomSpawn()
-    {
-        
+        for (int i = 0; i < numberOfPlatforms; i++)
+        {
+            spawnPosition.y += Random.Range(minY, maxY);
+            spawnPosition.x = Random.Range(-levelWidth, levelWidth);
+            Instantiate(platformPrefab, spawnPosition, Quaternion.identity);
+        }
     }
 }
